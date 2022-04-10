7d8536fdeb2315ecc62d5c1f067d6ef0250e19fb
Murat Can Kurt <kurt.muratcan@gmail.com>
2022-04-10 04:05:36 +0300

    UserRoles controller, view and a view model for user roles created.

---
+++ UserIdentityManagement.Web/Controllers/UserRolesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UserIdentityManagement.Web.Models;
+
+namespace UserIdentityManagement.Web.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UserRolesController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.ToListAsync();
+            var userRolesViewModel = new List<UserRolesViewModel>();
+            foreach (ApplicationUser user in users)
+            {
+                var thisViewModel = new UserRolesViewModel
+                {
+                    UserId = user.Id,
+                    Email = user.Email,
+                    UserName = user.UserName,
+                    FullName = $"{user.Name} {user.LastName}",
+                    Roles = await GetUserRoles(user)
+                };
+                userRolesViewModel.Add(thisViewModel);
+            }
+            return View(userRolesViewModel);
+        }
+
+        private async Task<List<string>> GetUserRoles(ApplicationUser user)
+        {
+            return new List<string>(await userManager.GetRolesAsync(user));
+        }
+    }
+}
---
+++ UserIdentityManagement.Web/Models/UserRolesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace UserIdentityManagement.Web.Models
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
