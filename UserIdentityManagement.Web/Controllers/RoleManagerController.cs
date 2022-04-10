dc09c76b13bd58669ec3881df4b9993ff22dd2ec
Murat Can Kurt <kurt.muratcan@gmail.com>
2022-04-10 03:26:59 +0300

    RoleManager Controller and View created.

---
+++ UserIdentityManagement.Web/Controllers/RoleManagerController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace UserIdentityManagement.Web.Controllers
+{
+    public class RoleManagerController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RoleManagerController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = await roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(string roleName)
+        {
+            if (!string.IsNullOrEmpty(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
